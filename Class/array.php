5e1b83c2346d4ac063dccd4010c186f95c465a0c
jamezilla0 <jamezilla0@gmail.com>
2015-04-02 12:28:31 -0400

    Update array.php

--- Class/array.php
+++ Class/array.php
@@ -1,25 +1,45 @@
-public function search($array,$key,$val)
+<?php
+class arrayFnc
 {
-//(string) Array key we will be working with
-$this->key = $key;
-//Key value that we need.
-$this->val = $val;
+	public $result = array();
+	
+	public function __construct()
+	{
+	}
 
-//Check if the passed in array is in fact an array
-if(is_array($array))
-{
-//if it is check if the specefied key matches the value
-if($array[$this->key] == $this->val)
-{
-//if so this result is the passed in array
-$this->result = $array;
+	public function search($array,$key,$val)
+	{
+		//(string) Array key we will be working with
+		$this->key = $key;
+		//Key value that we need.
+		$this->val = $val;
+
+		//Check if the passed in array is in fact an array
+		if(is_array($array))
+		{
+			//if it is check if the specefied key matches the value
+			if($array[$this->key] == $this->val)
+			{
+			//if so this result is the passed in array
+			$this->result = $array;
+			}
+			else
+			{
+			//other wise map the array to check each subarray for a match
+			array_map(array($this, "matchCheck"), $array);
+			//If the result is empty pass in false.
+			$this->result = (empty($this->result)) ? false : $this->result; 
+			}
+		}
+	}
+	
+	public function matchCheck($for)
+	{
+		if($for[$this->key] == $this->val)
+		{
+			array_push($this->result,$for);
+		}
+	}
+
 }
-else
-{
-//other wise map the array to check each subarray for a match
-array_map(array($this, "matchCheck"), $array);
-//If the result is empty pass in false.
-$this->result = (empty($this->result)) ? false : $this->result; 
-}
-}
-}
+?>
